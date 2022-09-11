04e0be3356fa6354ebabdf37c37d1da0474734e5
coheim <pari0130@gmail.com>
2022-09-12 01:44:03 +0900

    json save cache 주석 업데이트

--- lib/utils/cache_util.dart
+++ lib/utils/cache_util.dart
@@ -1,9 +1,17 @@
 
 import 'dart:convert';
 import 'dart:io';
+import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 
 class CacheUtil {
+  final logger = Logger();
+
+  /*
+  * 위도 경도에 대한 json 파일 저장
+  * 위도 경도에서 특정 수치만큼 (초) 거리가 범위안에 있을 경우는 해당 json 파일 return 아니면 새로 api 호출
+  * 37.480 3339 126.930 6995 에서 37.480 126.930 까지 같을 경우는 cache
+  * */
   static void saveJsonFile() async {
     var data =  {
       'data': 'null',
@@ -15,9 +23,21 @@ class CacheUtil {
     writeFileAsString(data: jsonString, path: 'test.json');
   }
 
-  static Future<File> writeFileAsString({ String? data, String? path }) async {
-    final file = await _localFile(path ?? 'cache/tmp.txt');
-    return file.writeAsString(data ?? '');
+  // static void saveJsonFile() async {
+  //   var data =  {
+  //     'data': 'null',
+  //     'map': {
+  //       'int': 0,
+  //     }
+  //   };
+  //   String jsonString = jsonEncode(data);
+  //   writeFileAsString(data: jsonString, path: 'test.json');
+  // }
+
+  static Future<File> writeFileAsString({ required String data, required String path }) async {
+    final file = await _localFile(path);
+    // 파일 읽기 file.readAsString();
+    return file.writeAsString(data);
   }
 
   static Future<File> _localFile(String morePath) async {
